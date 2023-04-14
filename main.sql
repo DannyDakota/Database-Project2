11082a774e5d2dcabfc6ee3b65a31c25ee8c9e4c
DannyDakota <51451719+DannyDakota@users.noreply.github.com>
2023-04-14 19:52:22 +0800

    testing

--- main.sql
+++ main.sql
@@ -2,10 +2,6 @@
 
 -- (1) Each delivery request has at least one package
 
-CREATE TRIGGER req_has_package
-BEFORE INSERT ON delivery_requests
-FOR EACH ROW EXECUTE FUNCTION check_packages_func();
-
 CREATE OR REPLACE FUNCTION check_packages_func() RETURNS TRIGGER
 AS $$
 begin
@@ -18,43 +14,67 @@ begin
 end;
 $$ LANGUAGE plpgsql;
 
+CREATE CONSTRAINT TRIGGER req_has_package
+after INSERT ON delivery_requests
+deferrable initially deferred
+FOR EACH ROW EXECUTE FUNCTION check_packages_func();
+
+CREATE OR REPLACE FUNCTION delivery_request_one_package_dr_func() 
+RETURNS TRIGGER AS $$ 
+BEGIN 
+IF NOT EXISTS (
+  SELECT
+    1
+  FROM
+    packages
+  WHERE
+    (request_id = NEW.id)
+) 
+THEN RAISE EXCEPTION 'Each delivery request needs to have at least one package!';
+
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER delivery_request_one_package_dr_check
+AFTER INSERT ON delivery_requests 
+DEFERRABLE INITIALLY DEFERRED 
+FOR EACH ROW EXECUTE FUNCTION delivery_request_one_package_dr_func();
+
 --------------------------------------------------------Package related---------------------------------------------------------------
 
 -- (2) For each delivery request, the IDs of the packages should be consecutive integers starting from 1.
 
-CREATE TRIGGER check_consecutive
-BEFORE INSERT ON delivery_requests
-FOR EACH ROW EXECUTE FUNCTION check_consecutive_func();
 
-CREATE OR REPLACE FUNCTION check_consecutive_func() 
+CREATE OR REPLACE FUNCTION check_consecutive_func() RETURNS TRIGGER 
 AS $$
-DECLARE max_package_id NUMERIC;
-begin
-    SELECT MAX(p.package_id) INTO max_package_id
-    FROM packages p
-    WHERE p.request_id = NEW.id;
-    IF max_package_id IS NOT NULL AND (max_package_id != COALESCE(NEW.package_id, 0) - 1) then
-        RAISE EXCEPTION 'IDs of the packages should be consecutive integers starting from 1';
-        RETURN NULL;
-    END IF;
-    IF max_package_id IS NULL AND NEW.package_id IS NULL then
+DECLARE max_id INT;
+BEGIN
+    SELECT MAX(package_id) FROM packages into max_id where packages.request_id = new.request_id;
+    max_id := max_id + 1;
+    IF (NEW.package_id = max_id) THEN
+        RETURN NEW;
+    ELSEIF (max_id is NULL) THEN
         NEW.package_id := 1;
+        RETURN NEW;
     END IF;
-    RETURN NEW;
-end;
+    RAISE EXCEPTION 'The IDs of the packages should be consecutive integers';
+    RETURN NULL;
+END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_consecutive
+BEFORE INSERT ON packages
+FOR EACH ROW EXECUTE FUNCTION check_consecutive_func();
 
 
 ------------------------------------------------------Unsuccessful_pickups related------------------------------------------------
 
 -- (3) For each delivery request, the IDs of the unsuccessful pickups should be consecutive integers starting from 1.
 
-CREATE TRIGGER check_pickup_trigger
-BEFORE INSERT ON unsuccessful_pickups
-FOR EACH ROW EXECUTE FUNCTION check_unsucc_pickup_func();
-
 CREATE OR REPLACE FUNCTION check_unsucc_pickup_func()
-AS $$
+returns  trigger AS $$
 DECLARE max_unsucc_pickup_id NUMERIC;
 begin
     SELECT MAX(pickup_id) INTO max_unsucc_pickup_id
@@ -71,39 +91,37 @@ begin
 end;
 $$ LANGUAGE plpgsql;
 
+CREATE or replace TRIGGER check_pickup_trigger
+BEFORE INSERT ON unsuccessful_pickups
+FOR EACH ROW EXECUTE FUNCTION check_unsucc_pickup_func();
+
 -----------------------------------------------------------------------------------------------------------------------------------
 
 /* (4) The timestamp of the first unsuccessful pickup should be after the submission_time of the 
 corresponding delivery request. In addition, each unsuccessful pickup’s timestamp should be after the 
 previous unsuccessful pickup’s timestamp (if any).*/
 
-CREATE TRIGGER check_pickup_timestamp_trigger
-BEFORE INSERT ON unsuccessful_pickups
-FOR EACH ROW EXECUTE FUNCTION check_pickup_timestamp_func();
-
 CREATE OR REPLACE FUNCTION check_pickup_timestamp_func()
-AS $$
+returns trigger AS $$
 DECLARE delivery_request_timestamp TIMESTAMP;
 DECLARE first_unsucc_pickup_timestamp TIMESTAMP;
 DECLARE last_unsucc_pickup_timestamp TIMESTAMP;
 begin
     SELECT d.submission_time into delivery_request_timestamp
-    FROM delivery_request d
+    FROM delivery_requests d
     WHERE NEW.request_id = d.id;
 
-    SELECT MIN(pickup_time) INTO first_unsucc_pickup_timestamp
+    SELECT pickup_time INTO last_unsucc_pickup_timestamp
     FROM unsuccessful_pickups u 
-    WHERE u.request_id = NEW.request_id;
+    WHERE u.request_id = NEW.request_id and u.pickup_id = new.pickup_id - 1;
 
-    SELECT MAX(pickup_time) INTO first_unsucc_pickup_timestamp
-    FROM unsuccessful_pickups u 
-    WHERE u.request_id = NEW.request_id;
-
-    IF (first_unsucc_pickup_timestamp < delivery_request_timestamp) then
-        RAISE EXCEPTION 'The timestamp of the first unsuccessful pickup should be after the submission_time of the 
-            corresponding delivery request';
-        return NULL;
-    END IF;
+   	if (new.pickup_id = 1) then
+		if (new.pickup_time < delivery_request_timestamp) then
+		RAISE EXCEPTION 'The timestamp of the first unsuccessful pickup should be after the submission_time of the 
+	            corresponding delivery request';
+	        return NULL;
+	    end if;
+   	end if;
     IF (NEW.pickup_time < last_unsucc_pickup_timestamp) then
     RAISE EXCEPTION 'Each unsuccessful pickup’s timestamp should be after the previous unsuccessful pickup’s timestamp';
         return NULL;
@@ -112,15 +130,13 @@ begin
 end;
 $$ LANGUAGE plpgsql;
 
+
 ------------------------------------------------------------Legs related--------------------------------------------------------------
 
 -- (5) For each delivery request, the IDs of the legs should be consecutive integers starting from 1. 
 
-CREATE TRIGGER check_legs_ID_trigger
-BEFORE INSERT ON legs
-FOR EACH ROW EXECUTE FUNCTION check_legs_ID_func();
 
-CREATE OR REPLACE FUNCTION check_legs_ID_func()
+CREATE OR REPLACE FUNCTION check_legs_ID_func() returns trigger
 AS $$
 DECLARE max_id INT;
 
@@ -138,19 +154,18 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE or replace TRIGGER check_legs_ID_trigger
+BEFORE INSERT ON legs
+FOR EACH ROW EXECUTE FUNCTION check_legs_ID_func();
 
 -------------------------------------------------------------------------------------------------------------------------------------
 
 /* (6) For  each  delivery  request,  the  start  time  of  the  first  leg  should  be  after  the  submission_time  of  the  
 delivery request and the timestamp of the last unsuccessful pickup (if any). */
 
-CREATE TRIGGER check_first_leg_start_time_trigger
-BEFORE INSERT ON legs
-WHEN (NEW.leg_id = 1)
-FOR EACH ROW EXECUTE FUNCTION check_first_leg_start_time_func();
-
 CREATE OR REPLACE FUNCTION check_first_leg_start_time_func()
-AS $$
+returns trigger AS $$
+
 DECLARE request_submission_time TIMESTAMP;
 DECLARE unsuccessful_pickup_timestamp TIMESTAMP;
 
@@ -172,16 +187,16 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE or replace TRIGGER check_first_leg_start_time_trigger
+BEFORE INSERT ON legs
+WHEN (NEW.leg_id = 1)
+FOR EACH ROW EXECUTE FUNCTION check_first_leg_start_time_func();
+
 ---------------------------------------------------------------------------------------------------------------------------------------------
 
 /* (7) For each delivery request, a new leg cannot be inserted if its start_time is before the end_time of the 
 previous leg, or if the end_time of the previous leg is NULL.*/
 
-CREATE TRIGGER check_legs_start_time_trigger
-BEFORE INSERT ON legs
-FOR EACH ROW 
-WHEN (NEW.leg_id > 1)
-EXECUTE FUNCTION check_legs_start_time_func();
 
 CREATE OR REPLACE FUNCTION check_legs_start_time_func()
 AS $$
@@ -202,18 +217,20 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER check_legs_start_time_trigger
+BEFORE INSERT ON legs
+FOR EACH ROW 
+WHEN (NEW.leg_id > 1)
+EXECUTE FUNCTION check_legs_start_time_func();
+
 ---------------------------------------------------------------Unsuccessful_deliveries----------------------------------------------------------------------
 
 /* (8) The timestamp of each unsuccessful_delivery should be after the start_time of the corresponding leg.*/
 
-CREATE TRIGGER check_unsuccessful_delivery_timestamp_trigger
-BEFORE INSERT ON unsuccessful_deliveries
-FOR EACH ROW 
-EXECUTE FUNCTION check_unsuccessful_delivery_timestamp_trigger_func();
 
 CREATE OR REPLACE FUNCTION check_unsuccessful_delivery_timestamp_trigger_func()
-AS $$
-DECLARE leg_start_time TIMESTAMP
+returns trigger AS $$
+DECLARE leg_start_time TIMESTAMP;
 BEGIN
     SELECT start_time FROM legs
     INTO leg_start_time
@@ -227,17 +244,18 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER check_unsuccessful_delivery_timestamp_trigger
+BEFORE INSERT ON unsuccessful_deliveries
+FOR EACH ROW 
+EXECUTE FUNCTION check_unsuccessful_delivery_timestamp_trigger_func();
+
 -------------------------------------------------------------------------------------------------------------------------------------
 /* (9) For each delivery request, there can be at most three unsuccessful_deliveries.*/
 
-CREATE TRIGGER check_num_of_unsuccessful_deliveries_trigger
-BEFORE INSERT ON unsuccessful_deliveries
-FOR EACH ROW
-EXECUTE FUNCTION check_num_of_unsuccessful_deliveries_func();
 
 CREATE OR REPLACE FUNCTION check_num_of_unsuccessful_deliveries_func()
-AS $$
-DECLARE curr_num_of_unsuccessful_deliveries INT
+returns trigger AS $$
+DECLARE curr_num_of_unsuccessful_deliveries INT;
 BEGIN
     SELECT COUNT(*) FROM unsuccessful_deliveries INTO curr_num_of_unsuccessful_deliveries
     WHERE request_id = NEW.request_id;
@@ -245,48 +263,50 @@ BEGIN
     IF curr_num_of_unsuccessful_deliveries >= 3 THEN
         RAISE EXCEPTION 'For each delivery request, there can be at most three unsuccessful_deliveries.';
         RETURN NULL;
-    END IF
+    END if;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_num_of_unsuccessful_deliveries_trigger
+BEFORE INSERT ON unsuccessful_deliveries
+FOR EACH ROW
+EXECUTE FUNCTION check_num_of_unsuccessful_deliveries_func();
 
 ------------------------------------------------------------Cancelled_requests related-----------------------------------------------
 
 /* (10) The cancel time of a cancelled request should be after the submission time of the corresponding delivery request.*/
 
-CREATE TRIGGER check_cancel_time_of_cancelled_request_trigger
-BEFORE INSERT ON cancelled_requests
-FOR EACH ROW
-EXECUTE FUNCTION check_cancel_time_of_cancelled_request_func();
 
 CREATE OR REPLACE FUNCTION check_cancel_time_of_cancelled_request_func()
-AS $$
-DECLARE delivery_request_submission_time TIMESTAMP
+returns trigger AS $$
+DECLARE delivery_request_submission_time TIMESTAMP;
 BEGIN
     SELECT submission_time FROM delivery_requests INTO delivery_request_submission_time
     WHERE id = NEW.id;
 
     IF NEW.cancel_time <= delivery_request_submission_time THEN
         RAISE EXCEPTION 'The cancel time of a cancelled request should be after the submission time of the corresponding  
-            delivery request.'
+            delivery request.';
         RETURN NULL;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER check_cancel_time_of_cancelled_request_trigger
+BEFORE INSERT ON cancelled_requests
+FOR EACH ROW
+EXECUTE FUNCTION check_cancel_time_of_cancelled_request_func();
+
 -------------------------------------------------------------Return_legs related---------------------------------------------------------------------
 
 /* (11) For each delivery request, the first return_leg’s ID should 1, the second return_leg’s ID should be 2, and so on.*/
 
-CREATE TRIGGER check_return_leg_id_trigger
-BEFORE INSERT ON return_legs
-FOR EACH ROW EXECUTE FUNCTION check_return_leg_id_func();
 
 CREATE OR REPLACE FUNCTION check_return_leg_id_func()
-AS $$
+returns trigger AS $$
 DECLARE current_leg_id_count NUMERIC;
-DECLARE correct_leg_id_count NUMERIC;
 BEGIN
     SELECT MAX(leg_id) INTO current_leg_id_count
     FROM return_legs l
@@ -295,22 +315,22 @@ BEGIN
         RETURN NEW;
     ELSIF current_leg_id_count IS NOT NULL AND (NEW.LEG_ID = current_leg_id_count + 1)then
         RETURN NEW;
-    ELSE
+    else
+    	RAISE EXCEPTION 'Return_leg id should be consecutive.';
         RETURN NULL;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-
+CREATE or replace TRIGGER check_return_leg_id_trigger
+BEFORE INSERT ON return_legs
+FOR EACH ROW EXECUTE FUNCTION check_return_leg_id_func();
 -------------------------------------------------------------------------------------------------------------------------------------
 
 /* (12) For a delivery request, the first return_leg cannot be inserted if (i) there is no existing leg for the delivery 
 request or (ii) the last existing leg’s end_time is after the start_time of the return_leg. In addition, the 
 return_leg’s start_time should be after the cancel_time of the request (if any). */
 
-CREATE TRIGGER check_return_leg_insertion_trigger
-BEFORE INSERT ON return_legs
-FOR EACH ROW EXECUTE FUNCTION check_return_leg_insertion_func();
 
 CREATE OR REPLACE FUNCTION check_return_leg_insertion_func();
 AS $$
@@ -324,7 +344,7 @@ request";
         RETURN NULL;
     END IF;
     -- TODO: Check if MAX(end_time) is correct, note that endtime can be null
-    SELECT MAX(end_time) INTO last_leg_timestamp
+    SELECT COALESCE(MAX(end_time),'1900-01-01 00:00:00')  INTO last_leg_timestamp
     FROM legs l
     WHERE l.request_id = NEW.request_id;
     IF last_leg_timestamp > NEW.start_time THEN 
@@ -332,11 +352,11 @@ request";
         RETURN NULL;
     END IF;
     
-    SELECT cancel_time INTO cancel_time_timestamp
+    SELECT COALESCE(cancel_time, '1900-01-01 00:00:00') INTO cancel_time_timestamp
     FROM cancelled_requests cr
     WHERE cr.id = NEW.request_id;
 
-    IF cancel_time_timestamp NOT NULL AND cancel_time_timestamp > NEW.start_time THEN
+    IF cancel_time_timestamp AND cancel_time_timestamp > NEW.start_time THEN
         RAISE EXCEPTION "Return leg's start time should be after cancel_time";
         RETURN NULL;
     END IF;
@@ -345,12 +365,13 @@ request";
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER check_return_leg_insertion_trigger
+BEFORE INSERT ON return_legs
+FOR EACH ROW EXECUTE FUNCTION check_return_leg_insertion_func();
+
 -------------------------------------------------------------------------------------------------------------------------------------
 
 /* (13) For each delivery request, there can be at most three unsuccessful_return_deliveries*/
-CREATE TRIGGER check_no_of_unsucc_return_deliv_trigger
-BEFORE INSERT ON unsuccessful_return_deliveries
-FOR EACH ROW EXECUTE check_no_of_unsucc_return_deliv_func();
 
 CREATE OR REPLACE FUNCTION check_no_of_unsucc_return_deliv_func()
 AS $$
@@ -360,7 +381,7 @@ BEGIN
     FROM unsuccessful_return_deliveries u
     WHERE new.request_id = u.request_id;
 
-    IF no_of_unsuccess > 2 THEN
+    IF no_of_unsuccess >= 3 THEN
         RAISE EXCEPTION 'Can only be at most three unsuccessful_return_deliveries';
         RETURN NULL;
     END IF;
@@ -368,15 +389,15 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
+
+CREATE TRIGGER check_no_of_unsucc_return_deliv_trigger
+BEFORE INSERT ON unsuccessful_return_deliveries
+FOR EACH ROW EXECUTE check_no_of_unsucc_return_deliv_func();
+
 ---------------------------------------------------------Unsuccessful_return_deliveries related--------------------------------------------------------
 
 /* (14) The timestamp of each unsuccessful_return_delivery should be after the start_time of the 
 corresponding return_leg. */
-
-CREATE TRIGGER check_unsuccessful_return_delivery_timestamp_trigger
-BEFORE INSERT ON unsuccessful_return_deliveries
-FOR EACH ROW
-EXECUTE FUNCTION check_unsuccessful_return_delivery_timestamp_func();
 
 CREATE OR REPLACE FUNCTION check_unsuccessful_return_delivery_timestamp_func()
 AS $$
@@ -394,4 +415,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+CREATE TRIGGER check_unsuccessful_return_delivery_timestamp_trigger
+BEFORE INSERT ON unsuccessful_return_deliveries
+FOR EACH ROW
+EXECUTE FUNCTION check_unsuccessful_return_delivery_timestamp_func();
---
+++ testing.sql
@@ -0,0 +1,248 @@
+--- Intialization of procedure for submit_request
+CREATE OR REPLACE PROCEDURE submit_request(
+  customer_id INTEGER,
+  evaluator_id INTEGER,
+  pickup_addr TEXT,
+  pickup_postal TEXT,
+  recipient_name TEXT,
+  recipient_addr TEXT,
+  recipient_postal TEXT,
+  submission_time TIMESTAMP,
+  package_num INTEGER,
+  reported_height INTEGER [],
+  reported_width INTEGER [],
+  reported_depth INTEGER [],
+  reported_weight INTEGER [],
+  content TEXT [],
+  estimated_value NUMERIC []
+) AS $$ 
+DECLARE 
+request_id INTEGER;
+temp_val INTEGER;
+
+BEGIN
+INSERT INTO
+  delivery_requests (
+    customer_id,
+    evaluater_id,
+    status,
+    pickup_addr,
+    pickup_postal,
+    recipient_name,
+    recipient_addr,
+    recipient_postal,
+    submission_time,
+    pickup_date,
+    num_days_needed,
+    price
+  )
+VALUES
+  (
+    customer_id,
+    evaluator_id,
+    'submitted',
+    pickup_addr,
+    pickup_postal,
+    recipient_name,
+    recipient_addr,
+    recipient_postal,
+    submission_time,
+    NULL,
+    NULL,
+    NULL
+  ) 
+  RETURNING id INTO request_id;
+
+temp_val := 1;
+
+LOOP 
+EXIT WHEN temp_val > package_num;
+
+INSERT INTO
+  packages (
+    request_id,
+    package_id,
+    reported_height,
+    reported_width,
+    reported_depth,
+    reported_weight,
+    content,
+    estimated_value,
+    actual_height,
+    actual_width,
+    actual_depth,
+    actual_weight
+  )
+VALUES
+  (
+    request_id,
+    temp_val,
+    reported_height [temp_val],
+    reported_width [temp_val],
+    reported_depth [temp_val],
+    reported_weight [temp_val],
+    content [temp_val],
+    estimated_value [temp_val],
+    NULL,
+    NULL,
+    NULL,
+    NULL
+  );
+
+temp_val := temp_val + 1;
+
+END LOOP;
+END;
+
+$$ LANGUAGE plpgsql;
+
+--- Insertion of testing data
+INSERT INTO Customers (name, gender, mobile) VALUES ('alice', 'female', '98765432');
+INSERT INTO Customers (name, gender, mobile) VALUES ('bob', 'male', '98765431');
+INSERT INTO Customers (name, gender, mobile) VALUES ('chris', 'female', '98765433');
+INSERT INTO Customers (name, gender, mobile) VALUES ('dylan', 'male', '98765434');
+
+INSERT INTO Employees (name, gender, dob, title, salary) VALUES ('employee1', 'female', '01-01-2000', 'evaluator', '3000');
+INSERT INTO Employees (name, gender, dob, title, salary) VALUES ('employee2', 'female', '01-01-2000', 'delivery man', '3000');
+INSERT INTO Employees (name, gender, dob, title, salary) VALUES ('employee3', 'male', '01-01-2000', 'manager', '3000');
+INSERT INTO Employees (name, gender, dob, title, salary) VALUES ('employee4', 'male', '01-01-2000', 'delivery man', '3000');
+INSERT INTO Employees (name, gender, dob, title, salary) VALUES ('employee5', 'male', '01-01-2000', 'nice guy', '3000');
+
+INSERT INTO Delivery_Staff VALUES (2);
+INSERT INTO Delivery_Staff VALUES (4);
+INSERT INTO Delivery_Staff VALUES (5);
+
+INSERT INTO Facilities (address, postal) VALUES ('facility address 1', '123123');
+INSERT INTO Facilities (address, postal) VALUES ('facility address 2', '123133');
+INSERT INTO Facilities (address, postal) VALUES ('facility address 3', '123143');
+INSERT INTO Facilities (address, postal) VALUES ('facility address 4', '123153');
+INSERT INTO Facilities (address, postal) VALUES ('facility address 5', '123163');
+
+--- Insert procedure for request
+CALL submit_request('1', '1', 'pickup_address', '123456', 'recipient_name', 'recipient_addr',
+  '654321', '2016-06-22 19:10:25-07', '2', ARRAY[2, 2], ARRAY[2, 2], ARRAY[2, 2],
+  ARRAY[2, 2], ARRAY['Content 1', 'Content 2'], ARRAY[2, 2]);
+
+--- [POSITIVE] Test that a consecutive package_id for the same delivery_request works
+INSERT INTO packages VALUES (3, 3, 12.5, 6.5, 10.2, 3.8, 'Pants', 50.0, 12.5, 6.5, 10.2, 3.8);
+
+--- [NEGATIVE] Test that a non-consecutive package_id for the same delivery_request raises exception
+INSERT INTO packages VALUES (3, 5, 12.5, 6.5, 10.2, 3.8, 'Pants', 50.0, 12.5, 6.5, 10.2, 3.8);
+
+--- [NEGATIVE] Test that a delivery_request with missing package raises exception
+INSERT INTO delivery_requests VALUES (1, 3, 1, 'completed', '123 Main St', '12345', 'John Doe', '456 Elm St', '67890', '2023-04-08 03:00:00', '2023-04-12', 3, 50.00);
+
+--- [POSITIVE] Test that a consecutive ID for an unsuccessful pickup works
+INSERT INTO accepted_requests VALUES(3, '333', '2022-04-11 03:00:00', 4);
+INSERT INTO unsuccessful_pickups VALUES(3, 1, 5, '2022-04-11 03:00:00', 'fail');
+
+--- [NEGATIVE] Test that a non-consecutive ID for an unsuccessful pickup raises exception
+INSERT INTO unsuccessful_pickups VALUES(3, 3, 5, '2022-04-11 03:00:00', 'fail');
+
+
+--- [POSITIVE] Test that the timestamp of the first unsuccessful pickup is after the submission_time and it works
+delete from unsuccessful_pickups where request_id = 3;
+insert into unsuccessful_pickups values(3, 1, 5, '2016-06-22 19:10:25.001', 'fail');
+
+--- [POSITIVE] Test that the timestamp of the first unssuccessful pickup is after the submission_time and subsequent pickup time after the prev works
+delete from unsuccessful_pickups where request_id = 3;
+insert into unsuccessful_pickups values(3, 1, 5, '2016-06-22 19:10:25.001', 'fail');
+insert into unsuccessful_pickups values(3, 2, 5, '2016-06-22 19:10:25.002', 'fail');
+
+--- [NEGATIVE] Test that the timestamp of the first unsuccessful pickup is before the submission_time and raises exception
+delete from unsuccessful_pickups where request_id = 3;
+insert into unsuccessful_pickups values(3, 1, 5, '2016-06-22 19:10:24.000', 'fail');
+
+--- [NEGATIVE] Test that the timestamp of the first unsuccessful pickup is after the submission_time and subsequent pickup time are before the prev and raises an exception
+delete from unsuccessful_pickups where request_id = 3;
+insert into unsuccessful_pickups values(3, 1, 5, '2016-06-22 19:10:25.007', 'fail');
+insert into unsuccessful_pickups values(3, 2, 5, '2016-06-22 19:10:24.003', 'fail');
+
+
+--- [POSITIVE] Test that a consecutive id for legs for the same delivery_request works
+INSERT INTO legs VALUES(3, 1, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+--- [NEGATIVE] Test that a non-consecutive id for legs for the same delivery_request raises an exception
+INSERT INTO legs VALUES(3, 2, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+--- [POSITIVE] Test that the timestamp of the first leg is after the submission_time and it works
+delete from legs where request_id = 3;
+INSERT INTO legs VALUES(3, 1, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+--- [POSITIVE] Test that the timestamp of the first leg is after the submission_time and last_unsuccessful_pick after the prev works
+delete from legs where request_id = 3;
+INSERT INTO legs VALUES(3, 1, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+--- [NEGATIVE] Test that the timestamp of the first leg is before the submission_time and raises exception
+delete from legs where request_id = 3;
+INSERT INTO legs VALUES(3, 1, 2, '2016-06-22 19:10:24.000', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+--- [NEGATIVE] Test that the timestamp of the first unsuccessful pickup is after the submission_time and start_time is before last_unsuccessful_pickup and raise an exception
+delete from legs where request_id = 3;
+INSERT INTO legs VALUES(3, 1, 2, '2016-06-22 19:10:25.005', '2023-04-18 10:35:00', 1); --(CUST,1)
+
+
+--- Test for trigger 7
+
+
+
+
+--- [POSITIVE] Time stamp of each unsuccessful_delivery should be after start_time of corresponding leg
+INSERT INTO legs VALUES(3, 2, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+INSERT INTO unsuccessful_deliveries VALUES (3, 1, 'Fell asleep', '2023-04-18 11:00:00');
+INSERT INTO unsuccessful_deliveries VALUES (3, 2, 'Fell asleep', '2023-04-18 12:00:00');
+
+--- [NEGATIVE] Timestamp fo each unsuccessful_delivery is before and raises an exception
+delete from unsuccessful_deliveries where request_id = 3;
+INSERT INTO unsuccessful_deliveries VALUES (3, 1, 'Fell asleep', '2016-06-22 19:09:24.000');
+INSERT INTO unsuccessful_deliveries VALUES (3, 2, 'Fell asleep', '2016-06-22 19:09:24.000');
+
+
+--- [NEGATIVE] More than three unsuccessful_delivery raises an exception
+INSERT INTO legs VALUES(3, 3, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+INSERT INTO legs VALUES(3, 4, 2, '2023-04-18 10:00:00', '2023-04-18 10:35:00', 1); --(CUST,1)
+delete from unsuccessful_deliveries where request_id = 3;
+INSERT INTO unsuccessful_deliveries VALUES (3, 1, 'Fell asleep', '2023-04-18 11:00:00');
+INSERT INTO unsuccessful_deliveries VALUES (3, 2, 'Fell asleep', '2023-04-18 11:00:00');
+INSERT INTO unsuccessful_deliveries VALUES (3, 3, 'Fell asleep', '2023-04-18 11:00:00');
+--- This one im a bit unsure, can there be a leg with id 4?
+INSERT INTO unsuccessful_deliveries VALUES (3, 4, 'Fell asleep', '2023-04-18 11:00:00');
+
+
+
+--- [POSITIVE] cancel_time after submission_time and works
+insert into cancelled_requests values (3, '2023-04-18 10:00:00');
+
+--- [NEGATIVE] cancel_time after submission_time and raises an exception
+delete from cancelled_requests where id = 3;
+insert into cancelled_requests values (3, '2016-06-22 19:10:24.000');
+
+
+--- [POSITIVE] For each delivery request, first = 1, second = 2, ...
+INSERT INTO cancelled_or_unsuccessful_requests VALUES (3);
+INSERT INTO return_legs VALUES(3, 1, 2, '2023-04-24 10:05:06', 1, '2023-04-24 10:35:06');
+INSERT INTO return_legs VALUES(3, 2, 2, '2023-04-24 10:05:06', 1, '2023-04-24 10:35:06');
+INSERT INTO return_legs VALUES(3, 3, 2, '2023-04-24 10:05:06', 1, '2023-04-24 10:35:06');
+
+
+--- [NEGATIVE] first = 1, second = 3
+delete from return_legs where request_id  = 3;
+INSERT INTO return_legs VALUES(3, 1, 2, '2023-04-24 10:05:06', 1, '2023-04-24 10:35:06');
+INSERT INTO return_legs VALUES(3, 3, 2, '2023-04-24 10:05:06', 1, '2023-04-24 10:35:06');
+
+
+--- Trigger 12
+
+--- Trigger 13
+--- My trigger might be wrong as it perform a before insert on unsucces_return instead of return_legs
+--- [NEGATIVE] More than three unsuccessful_return_deliveries
+
+
+--- [POSITIVE] Timestamp of each unsuccessful_return_delivery should be after the start_time of the corresponding return_leg and works
+insert into unsuccessful_return_deliveries values (3, 1, 'toh', '2023-04-24 10:05:10.000');
+
+--- [NEGATIVE] Timestamp of each unsuccessful_return_delivery before the start_time of the corresponding return_leg and raises exception
+delete from unsuccessful_deliveries where request_id = 3;
+insert into unsuccessful_return_deliveries values (3, 1, 'toh', '2023-04-24 10:00:00.000');
+
+
